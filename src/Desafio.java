9ace69291b43de95826cdfbb546209f0a3356659
Daniele Inspiron <contatodanny@hotmail.com>
2025-04-26 21:39:29 -0300

    ifs para atribuir ação ao menu

--- src/Desafio.java
+++ src/Desafio.java
@@ -25,6 +25,26 @@ public class Desafio {
         while (opcao !=4) {
             System.out.println(menu);
             opcao = leitura.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("Saldo: " + saldo);
+            }else if (opcao == 2) {
+                System.out.println("Digite o valor da transferencia: ");
+                double valor = leitura.nextDouble();
+                if(valor > saldo){
+                    System.out.println("Saldo insuficiente");
+                }else {
+                    saldo -= valor;
+                    System.out.println("Saldo: " + saldo);
+                }
+            }else if (opcao == 3) {
+                System.out.println("Valor recebido:");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Saldo: " + saldo);
+            }else if (opcao != 4) {
+                System.out.println("Opção invalida");
+            }
         }
     }
 }
