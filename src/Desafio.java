ba2bc902630f829e69adbcc93da0c73b7525a84f
Daniele Inspiron <contatodanny@hotmail.com>
2025-04-26 21:15:17 -0300

    menu

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Daniele";
+        String tipoConta = "Corrente";
+        double saldo = 5000;
+        int opcao = 0;
+
+        System.out.println("************************");
+        System.out.println("\nOlá, " + nome);
+        System.out.println("Conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("\n************************");
+
+        String menu = """
+                ** Digite a dua opção **
+                1 - Consultar Saldo
+                2 - Tranferencia
+                3 - Receber valor
+                4 - Sair
+                """;
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao !=4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
+    }
 }
